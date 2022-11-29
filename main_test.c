61e0c67d7f8125c69a33d894590662cf484a1907
yoji <106401357+TheNightJourney@users.noreply.github.com>
2022-11-29 22:26:10 +0700

    Update main_test.c

--- main_test.c
+++ main_test.c
@@ -7,6 +7,8 @@ int main(void){
     int N = 10;
 
     printf("%d = %d\n",fibonacciIterative(N),fibonacciRecursive(N));
+    printf("The Iterative result for %d values is %d.\n",N,fibonacciIterative(N));
+    printf("The Recursive approach for %d values is %d.",N,fibonacciRecursive(N));
 
     return 0;
 }
