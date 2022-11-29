d7f19af8fc0ab5c54d4090019153ec23fd3d5ec1
yoji <106401357+TheNightJourney@users.noreply.github.com>
2022-11-29 23:13:36 +0700

    Update mylib.c

--- mylib/mylib.c
+++ mylib/mylib.c
@@ -1,35 +1,5 @@
 #include <stdio.h>
 
-int factorialIterative(int N){
-    int output = 1;
-
-    /* "for i = 1" denotes the start value, the following 
-        denotes the stop.
-        in c, "i++" is "i = i+1"
-        */
-    for(int i = 1; i <= N; i++){
-        output = output*i;
-    }
-
-    return output;
-
-    /*
-        for i in range(N):
-            output = output*i
-     */
-
-}
-
-int factorialRecursive(int N){
-    int output = 1;
-
-    if (N == 1){
-            return 1;
-    } else {
-        return N*factorialRecursive(N-1);
-    }
-}
-
 int fibonacciIterative(int N){
     int output = 1, initial = 0, secondary = 1, final, i;
     for (i = 0; i < N; i++){
