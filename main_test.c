e6e72245272b4f046b9174887e1bba568a9ae0fd
yoji <106401357+TheNightJourney@users.noreply.github.com>
2022-11-29 22:12:03 +0700

    Update main_test.c

--- main_test.c
+++ main_test.c
@@ -6,7 +6,6 @@ int main(void){
 
     int N = 10;
 
-    printf("%d = %d = %d\n",N,factorialIterative(N),factorialRecursive(N));
     printf("%d = %d\n",fibonacciIterative(N),fibonacciRecursive(N));
 
     return 0;
